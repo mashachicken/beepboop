90967d51ca5580d71ce41fb5bd6ef0c62c9b089c
masha <mashanechmo@gmail.com>
2021-06-11 17:11:32 -0700

    add more css

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ function beepBoop(numberInput) {
   var newArray = [];
   for (var i=0; i<=numberInput; i++) {
     if (i.toString().includes("3")) {
-      newArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      newArray.push("Won't you be my neighbor?.");
     } else if (i.toString().includes("2")) {
       newArray.push("boop!")
     } else if (i.toString().includes("1")) {
