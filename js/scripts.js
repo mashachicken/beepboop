66c2b5d0825db023d82d9a333422da4f85b1c9a0
masha <mashanechmo@gmail.com>
2021-06-13 12:21:02 -0700

    add conditionals that will only return one output for numbers that include 3, 2 or 1 in the most to least importnat order

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,39 @@
-$(document).ready(function(){
-  $("form#enterNumber").submit(function(event) {
+$(document).ready(function () {
+  $("form#enterNumber").submit(function (event) {
     event.preventDefault();
     var numberInput = parseInt($("input").val());
     var result = beepBoop(numberInput)
-    $("#responces1").html(result)
     console.log(result)
-    });
+    $("#responces1").html(result)
+  });
 });
 function beepBoop(numberInput) {
   var newArray = [];
-  for (var i=0; i<=numberInput; i++) {
-    if (i.toString().includes("3")) {
-      newArray.push("Won't you be my neighbor?.");
-    } else if (i.toString().includes("2")) {
-      newArray.push("boop!")
-    } else if (i.toString().includes("1")) {
-      newArray.push("beep!")
-    } else {
-      newArray.push(i);
-}
-};
-return newArray;
+  if ((numberInput.toString().split('')).includes("3")){
+    newArray.push("Won't you be my neighbor?");
+    console.log(newArray)
+  }
+  else if ((numberInput.toString().split('')).includes("2")){
+    newArray.push("boop!")
+  }
+  else if ((numberInput.toString().split('')).includes("1")) {
+    newArray.push("beep!")
+  } else {
+    for (var i = 0; i <= numberInput; i++) {
+      if (i.toString().includes("3")) {
+        newArray.push("Won't you be my neighbor?");
+      } else if (i.toString().includes("2")) {
+        newArray.push("boop!")
+      } else if (i.toString().includes("1")) {
+        newArray.push("beep!")
+      } else {
+        newArray.push(i + ',');
+      }
+    };
+    return newArray;
+
+  };
+  console.log(newArray)
+  return newArray;
+
 };
